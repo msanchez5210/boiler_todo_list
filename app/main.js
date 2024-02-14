1cde0338488c8e46cdda445f06867ff5ba5767b8
msanchez5210 <msanchez5210@esdmadrid.es>
2024-02-14 18:36:11 +0100

    primera version para añadir

--- app/main.js
+++ app/main.js
@@ -3,7 +3,7 @@ import { tareas } from '../assets/data/tareas.js';
 window.onload = () => {
     let lista = document.querySelector("#lista");
 
-    tareas.forEach((tarea) => {
+    function agregarElementoTarea(tarea) {
         let elem = `
             <li class="lista_elemento" id="${tarea.id}">
                 <p>${tarea.titulo}</p>
@@ -12,6 +12,27 @@ window.onload = () => {
         `;
         console.log(elem)
         lista.innerHTML+=elem;
-    })
-}
+    };
+
+    function agregarNuevaTarea() {
+        let nuevaTareaInput = document.getElementById('nuevaTarea');
+        let nuevaTarea = nuevaTareaInput.value.trim();
+
+        if (nuevaTarea !== "") {
+            let elemento = {titulo: nuevaTarea};
+            agregarElementoTarea(elemento);
+            nuevaTareaInput.value = "";
+        }
+    };
+
+    tareas.forEach((tarea) => {
+        agregarElementoTarea(tarea);
+    });
+
+    let formulario = document.getElementById('formulario');
+    formulario.addEventListener('submit', (event) => {
+        event.preventDefault();
+        agregarNuevaTarea();
+    });
+};
 
